24265af811134decf4840538d41ede2fbbd1516c
Lyud0k <99692275+Lyud0k@users.noreply.github.com>
2022-09-16 02:14:41 +0300

    hw11_pAxios

--- src/index.js
+++ src/index.js
@@ -16,21 +16,50 @@ findClick.addEventListener('click', searchMore);
 findClick.disabled = true;
 let num = 1;
 
+const BASE_URL = "https://pixabay.com/api/"
+const key = "29818615-eeef91044a0285c2bbb309d67"
+const imageType = 'photo';
+const orientation = 'horizontal';
+const safesearch = 'true';
+
 class NewCont {
   constructor() {
-    this.name = '';
+    this.searchQuery = '';
     this.page = 1;
   }
 
-  fetchCont() {
-    console.log(this.data);
-    const url = `https://pixabay.com/api/?key=29818615-eeef91044a0285c2bbb309d67&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
-    return fetch(url).then(response => response.json())
-      .then(data => {
+  // fetchCont() {
+  //   const url =
+  //     axios.get(`https://pixabay.com/api/?key=29818615-eeef91044a0285c2bbb309d67&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+  //   return fetch(url).then(response => response.json())
+  //     .then(data => {
+  //       this.incrementPage();
+  //       return data;
+  //     });
+  // }
+
+  async getCont() {
+    const options = {
+      params: {
+      key,
+      q: this.searchQuery,
+      image_type: imageType,
+      orientation,
+      safesearch,
+      page: this.page,
+      per_page: 40,
+      }
+      }
+  try {
+    const data = await axios.get(BASE_URL, options);
+        
         this.incrementPage();
         return data;
-      });
+      
+  } catch (error) {
+    console.error(error);
   }
+}
   
     incrementPage() {
     this.page += 1;
@@ -47,37 +76,23 @@ class NewCont {
 }
 
 const newCont = new NewCont();
-// const pr = newCont.fetchCont();
-// console.log(pr);
-// const et = pr.totalHits;
-// console.log(et);
 
 function searchPicture(evt) {
   evt.preventDefault();
   newCont.query = evt.currentTarget.searchQuery.value;
   newCont.resetPage();
-  newCont.fetchCont().then(data => outputPictures(data.hits));
+  newCont.getCont().then(data => outputPictures(data));
 }
 
 function searchMore() {
-  newCont.fetchCont().then(data => outputPictures(data.hits));
-  console.log(data.totalHits);
-  
+  newCont.getCont().then(data => outputPictures(data));
+  // newCont.incrementPage();
 }
 
-function outputPictures(pictures) {
-  // const data = newCont.fetchCont();
-  // const hit = data.totalHits;
-  // console.log(hit);
-
-  
-  // const put = newCont.fetchCont();
-  // console.log(put);
-  // const data = put.data;
-  // console.log(data);
-  // const kolTotal = data.totalHits;
-  // console.log(kolTotal);
-
+async function outputPictures(pictures) {
+  // const data = await newCont.getCont();
+  const allPictures = data.hits;
+  const amountPictures = data.totalHits;
   findClick.disabled = false;
   if (pictures.length === 0) {
        Notify.failure(
@@ -85,10 +100,11 @@ function outputPictures(pictures) {
     );
     findClick.disabled = true;
   }
-     const markup = pictures
+     const markup = allPictures
          .map((picture) => {num += 1;
-        return `<div class="photo-card">
-    <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy"  class="gallery__image" />
+           return `<div class="photo-card">
+        <a href="${picture.largeImageURL}">
+    <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy"  class="gallery__image" /></a>
     <div class="info">
       <p class="info-item">
         <b>Likes </b>${picture.likes}</p>
@@ -102,8 +118,7 @@ function outputPictures(pictures) {
   </div>`;    
     })
        .join("");
-  findDiv.innerHTML = markup;
-  // console.log(pictures.length);
+  // findDiv.innerHTML = markup;
     num -= 1;
   if (num < 40) {
     findClick.disabled = true;
@@ -111,4 +126,26 @@ function outputPictures(pictures) {
       "We're sorry, but you've reached the end of search results."
     );
   }
+ notification(num, amountPictures);
+  num = 1;
+  if (newCont.page === 1) {
+    findDiv.innerHTML = markup;
+  }
+  if (newCont.page !== 1) {
+    findDiv.insertAdjacentHTML('beforeend', markup);
+  }
+  let gallery = new SimpleLightbox('.photo-card a');
+findDiv.addEventListener('click', evt => {
+  evt.preventDefault();
+})
+gallery.on('show.simplelightbox', function () {
+	gallery.defaultOptions.captionDelay = 250;
+});
+gallery.refresh();
 };
+function notification(totalImg, totalHits) {
+  if (newCont.page > 1 && totalImg === 40) {
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+  }
+}
+
