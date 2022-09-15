2e979df163146f2bd08ed36089a2476ad7823f92
Lyud0k <99692275+Lyud0k@users.noreply.github.com>
2022-09-15 21:59:22 +0300

    hw11_pTotal

--- src/index.js
+++ src/index.js
@@ -7,26 +7,31 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const findSubmit = document.querySelector('button[type="submit"]');
 const findInput = document.querySelector('input[name="searchQuery"]');
 const findDiv = document.querySelector('.all_box');
-const findClick = document.querySelector('button[type="button"]');
+const findClick = document.querySelector('.load-more');
 const findForm = document.querySelector('.search-form');
 
 findForm.addEventListener('submit', searchPicture);
 findClick.addEventListener('click', searchMore);
+
+findClick.disabled = true;
+let num = 1;
 
 class NewCont {
   constructor() {
     this.name = '';
     this.page = 1;
   }
-  
+
   fetchCont() {
+    console.log(this.data);
     const url = `https://pixabay.com/api/?key=29818615-eeef91044a0285c2bbb309d67&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
     return fetch(url).then(response => response.json())
       .then(data => {
         this.incrementPage();
-        return data.pictures;
+        return data;
       });
   }
+  
     incrementPage() {
     this.page += 1;
   }
@@ -42,22 +47,46 @@ class NewCont {
 }
 
 const newCont = new NewCont();
+// const pr = newCont.fetchCont();
+// console.log(pr);
+// const et = pr.totalHits;
+// console.log(et);
 
 function searchPicture(evt) {
   evt.preventDefault();
   newCont.query = evt.currentTarget.searchQuery.value;
   newCont.resetPage();
-  newCont.fetchCont().then(outputPictures);
+  newCont.fetchCont().then(data => outputPictures(data.hits));
 }
 
 function searchMore() {
-   newCont.fetchCont().then(outputPictures);
+  newCont.fetchCont().then(data => outputPictures(data.hits));
+  console.log(data.totalHits);
+  
 }
 
 function outputPictures(pictures) {
-  console.log(pictures);
+  // const data = newCont.fetchCont();
+  // const hit = data.totalHits;
+  // console.log(hit);
+
+  
+  // const put = newCont.fetchCont();
+  // console.log(put);
+  // const data = put.data;
+  // console.log(data);
+  // const kolTotal = data.totalHits;
+  // console.log(kolTotal);
+
+  findClick.disabled = false;
+  if (pictures.length === 0) {
+       Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    findClick.disabled = true;
+  }
      const markup = pictures
-         .map((picture) => {
+         .map((picture) => {num += 1;
         return `<div class="photo-card">
     <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy"  class="gallery__image" />
     <div class="info">
@@ -73,5 +102,13 @@ function outputPictures(pictures) {
   </div>`;    
     })
        .join("");
-    findDiv.innerHTML = markup;
+  findDiv.innerHTML = markup;
+  // console.log(pictures.length);
+    num -= 1;
+  if (num < 40) {
+    findClick.disabled = true;
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 };
